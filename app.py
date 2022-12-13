8dc2c9296806303b78d9c3cf7d9fee94bb2feb8e
dimka77783 <dimka77783@mai.ru>
2022-12-13 22:58:18 +0300

    1

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, url_for, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return redirect(url_for('about'))
+    #return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ =='__main__':
+    app.run(debug=True, port=5001)
