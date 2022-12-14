97f9488a8150f2fc1257a938f59a6446565bbd08
dimka77783 <dimka77783@mai.ru>
2022-12-14 22:37:33 +0300

    база db

--- app.py
+++ app.py
@@ -1,15 +1,37 @@
 from flask import Flask, render_template, url_for, redirect
+from flask_bootstrap import Bootstrap
+from flask_mysqldb import MySQL
+import yaml 
 
 app = Flask(__name__)
+Bootstrap(app)
+
+#DB configuration
+db = yaml.load(open('db.yaml'))
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
+mysql = MySQL(app)
 
 @app.route('/')
 def index():
-    return redirect(url_for('about'))
-    #return render_template('index.html')
+    cursor = mysql.connection.cursor()
+    #cursor.execute('INSERT INTO user VALUES(%s)',(['John']))
+    #mysql.connection.commit()
+    resut = cursor.execute('SELECT*FROM user')
+    if resut>0:
+        users = cursor.fetchall()
+        print(users)
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+@app.route('/css')
+def css():
+    return render_template('css.html')
+
 if __name__ =='__main__':
-    app.run(debug=True, port=5001)
+    app.run(debug=True, port=5000)
