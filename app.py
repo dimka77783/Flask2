ee7352dd9e589072d22fdcb59b8a51bdb850ca5f
dimka77783 <dimka77783@mai.ru>
2022-12-19 22:06:45 +0300

    Fishi

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template, url_for, redirect, request, session
+from flask import Flask, render_template, url_for, redirect, request, session, flash
 from flask_bootstrap import Bootstrap
 from flask_mysqldb import MySQL
-import yaml 
+import yaml, secrets
 import os
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 Bootstrap(app)
+secret = secrets.token_urlsafe(32)
 
 #DB configuration
 db = yaml.load(open('db.yaml'))
@@ -15,20 +16,26 @@ app.config['MYSQL_USER'] = db['mysql_user']
 app.config['MYSQL_PASSWORD'] = db['mysql_password']
 app.config['MYSQL_DB'] = db['mysql_db']
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+app.secret_key = secret
 mysql = MySQL(app)
 
-app.config['SECRET_KEY'] = os.urandom(24)
+#app.config['SECRET_KEY'] = os.urandom(24)
 
 @app.route('/', methods=['GET','POST'])
 def index():
     if request.method == 'POST':
-        form = request.form
-        name = form['name']
-        age = form['age']
-        cursor = mysql.connection.cursor()
-        name = generate_password_hash(name)
-        cursor.execute("INSERT INTO employee(name, age) VALUES(%s, %s)", (name, age))
-        mysql.connection.commit()
+        try:
+            form = request.form
+            name = form['name']
+            age = form['age']
+            cursor = mysql.connection.cursor()
+            # кеширование
+            #  name = generate_password_hash(name)
+            cursor.execute("INSERT INTO employee(name, age) VALUES(%s, %s)", (name, age))
+            mysql.connection.commit()
+            flash('OK', 'success')
+        except:
+            flash('NOT!!!!', 'danger')
     return render_template('index1.html')
 
 @app.route('/employees')
